5e1280eebed4b81bbf1416e37c9f564641ea9be9
cqf1991 <cqf1324@vip.qq.com>
2015-07-06 20:56:18 +0800

    简单但是蛋疼的递归，以后递归练习不再新建文件，全放在recursionPractice.py里

---
+++ recursionPractice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+### 一个蛋疼简单的递归
+
+
+def exR(n):
+    if n<=0:
+        return ''
+    return exR(int(n)-3),n,exR(int(n)-2),n
+###   answer 3 1 1 3 6 1 1 4 2 2 6
+
+print exR('6')
