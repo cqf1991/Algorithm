75fff2faceab30cc31920aa4c636567c96f3e6b3
cqf1991 <cqf1324@vip.qq.com>
2015-07-07 20:34:11 +0800

    Practice ln(N！)

--- recursionPractice.py
+++ recursionPractice.py
@@ -9,4 +9,15 @@ def exR(n):
     return exR(int(n)-3),n,exR(int(n)-2),n
 ###   answer 3 1 1 3 6 1 1 4 2 2 6
 
-print exR('6')
+###print exR('6')
+
+
+import math
+
+def RECcalcLN(x):
+    if x==0:
+        return 1
+    return  x*RECcalcLN(x-1)
+
+
+print math.log(RECcalcLN(2))
