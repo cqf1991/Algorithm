e9126e6f66cb9059b248e427209add048a9f4026
cqf1991 <cqf1324@vip.qq.com>
2015-07-15 00:04:53 +0800

    linklist 1st ver

---
+++ linklist.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+### implement linklist by python
+
+class Node:
+    next=None
+    data=None
+    def __init__(self,nodedata):
+        self.data=nodedata
+        
+
+class linklist:
+    first=None
+    current=None
+    size=0
+    def __init__(self,newNode=None):
+        if newNode!=None:
+            self.first=newNode
+            self.current=newNode
+            self.size=1
+        else:
+            print 'u have init a empty linklist'
+    
+    def insert(self,newNode):
+        insertNode=Node(newNode)
+        if self.first==None:
+            self.first=insertNode
+            self.current=insertNode
+            self.size+=1
+            print 'add a newNode in the first position'
+        else:
+            self.current.next=insertNode
+            self.current=insertNode
+            self.size+=1
+            print 'add a newNode at the end '
+
+
+    def getAll(self):
+        for i in range(0,self.size):
+
+            print 'data:',self.first.next.next.data
+            print self.size
+
+    
+
+la=linklist()
+la.insert('abc')
+la.insert('123')
+la.insert('777777')
+
+la.getAll()
+
