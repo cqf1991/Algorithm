203dfc1b21ddd6f06c04924ed2b6b4d92fb5910d
稀饭微凉~ <cqf1324@vip.qq.com>
2015-07-25 10:48:30 +0800

    ver1.0
    python实现算法第一课的percolation问题，尚未考虑"回流"问题，还需修改，还需添加蒙特卡洛分析方法的实现

---
+++ Coursera_Algortihm_Week1_ProgrammingAssignment/Percolation.py
@@ -0,0 +1,72 @@
+﻿import WeightedQuickUnionUF
+class Percolation:
+    count=0
+    WQUF=None
+    sideN=0
+    isOpen=[]
+    def __init__(self,N):
+        self.count=N*N
+        self.sideN=N
+        self.WQUF=WeightedQuickUnionUF.WeightedQuickUnionUF(N*N+2)
+        self.isOpen=[ False for i in range(N*N+1)] #give up isOpen[0]
+        self.isOpen[0]=0
+        for i in range(1,N+1):
+            self.WQUF.union(0,i)
+        for i in range(N*N,N*N-N,-1):
+            self.WQUF.union(N*N+1,i)
+
+        
+
+    def open(self,i,j):
+        self.validate(i)
+        self.validate(j)
+        subX=i-1   # column i & j  -> subscript
+        subY=j-1
+        nowPosition=subX*self.sideN+j
+        if self.isOpen[nowPosition] is False:
+            self.isOpen[nowPosition]=True
+            if i>1 and self.isOpen[nowPosition-self.sideN] is True:
+                self.WQUF.union(nowPosition-self.sideN,nowPosition)
+            if i<self.sideN  and self.isOpen[nowPosition+self.sideN] is True:
+                self.WQUF.union(nowPosition+self.sideN,nowPosition)
+            if j>1 and self.isOpen[nowPosition-1] is True:
+                self.WQUF.union(nowPosition-1,nowPosition)
+            if j<self.sideN and self.isOpen[nowPosition+1] is True:
+                self.WQUF.union(nowPosition+1,nowPosition)
+
+
+
+    def validate(self,P):
+        if(P<1 or P>self.sideN):
+            print 'validate failed'
+            return False
+        return True
+
+
+    def isOpen(self,i,j):
+        if self.validate(i) and self.validate(j):
+            return self.isOpen[(i-1)*self.sideN+j]
+
+      
+    def isFull(self,i,j):
+        if self.validate(i) and self.validate(j) and self.isOpen(i,j):
+            return self.WQUF.connected(0,(i-1)*self.sideN+j)
+
+
+    def percolates(self):
+        return self.WQUF.connected(0,self.sideN*self.sideN+1)
+
+
+pe=Percolation(5)
+print pe.WQUF.parent
+pe.open(2,3)
+pe.open(3,2)
+pe.open(3,4)
+pe.open(4,3)
+pe.open(3,3)
+pe.open(1,3)
+pe.open(4,4)
+pe.open(5,5)
+print pe.isOpen
+print pe.WQUF.parent
+print pe.percolates()
---
+++ Coursera_Algortihm_Week1_ProgrammingAssignment/WeightedQuickUnionUF.py
@@ -0,0 +1,56 @@
+﻿
+
+class WeightedQuickUnionUF:
+    count=0
+    parent=[] #id[i]
+    size=[]   #size[i]
+    def __init__(self,N=0):
+        self.count=N
+        self.parent=[ i for i in range(N)]  # init list [0,1,2,3...]
+        self.size=[ 1 for i in range(N)]       
+
+    def validate(self,P):
+        if(P<0 or P>self.count):
+            print 'validate failed'
+            return False
+        return True
+
+    def find(self,p):
+        if self.validate(p):
+            while(p!=self.parent[p]):#!!!
+                p=self.parent[p]
+            return p
+    
+    def connected(self,p,q):
+        return self.find(p)==self.find(q)
+
+    def union(self,p,q):
+        rootP=self.find(p)
+        rootQ=self.find(q)
+        if(rootP==rootQ):
+            return
+        if self.size[rootP]<self.size[rootQ]:
+            self.parent[rootP]=rootQ
+            self.size[rootP]+=self.size[rootQ]
+        else:
+            self.parent[rootQ]=rootP
+            self.size[rootP]+=self.size[rootQ]
+      
+
+
+
+'''
+xb=WeightedQuickUnionUF(10)
+xb.union(4,3)
+xb.union(3,8)
+xb.union(6,5)
+xb.union(9,4)
+xb.union(2,1)
+xb.union(5,0)
+xb.union(7,2)
+xb.union(6,1)
+xb.union(7,3)
+print xb.parent   
+print xb.size
+print xb.connected(3,5)
+'''
